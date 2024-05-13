662bb9544818d2c5f45fbd066510dddd734bfa08
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-05-13 17:21:09 +0200

    test img

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,35 @@
 <script>
 
+export default {
+  name: 'AppHeader'
+}
+
 </script>
 
 <template>
-    <div>
-        HEADER
+    <div class="header">
+    <div class="header-top">
+      <div class="logo">
+        <img src="" alt="Logo">
+        <span>Avada Pet Supplies</span>
+      </div>
+      <div class="contact-info">
+        <span>Questions? Call us: 1.800.123.4567</span>
+      </div>
+    </div>
+    <nav class="nav-bar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/shop">Shop</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/brand">Shop by brand</a></li>
+      </ul>
+    </nav>
     </div>
 </template>
 
-<style>
+<style scoped lang="scss">
 
 </style>
