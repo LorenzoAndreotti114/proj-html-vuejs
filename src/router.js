fa54e1cbe2eeb7d8c5692b587701a43b36cd6bac
LorenzoAndreotti114 <lorenzoandr123@gmail.com>
2024-05-16 13:37:31 +0200

    bug fixing

--- src/router.js
+++ src/router.js
@@ -1,16 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage.vue";
 import ContactPage from "./pages/ContactPage.vue";
 import AboutPage from "./pages/AboutPage.vue";
 import ShopPage from "./pages/ShopPage.vue";
-=======
-import HomePage from "./pages/HomePage.vue"
-import ContactPage from "./pages/ContactPage.vue"
-import AboutPage from "./pages/AboutPage.vue"
-import ShopPage from "./pages/ShopPage.vue"
->>>>>>> 3e7fbecfcf311435919f0a9b38d7ddf9931c159d
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -36,14 +29,10 @@ export const router = createRouter({
             name: "Contact",
             component: ContactPage,
         },
-<<<<<<< HEAD
         {
             path: "/shop",
             name: "Shop",
             component: ShopPage,
         },
-=======
-        
->>>>>>> 3e7fbecfcf311435919f0a9b38d7ddf9931c159d
     ]
 })
